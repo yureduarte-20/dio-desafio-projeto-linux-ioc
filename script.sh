36f8974a505e50b3aab91e4037f4643df61a7710
Yure Samarone <yuresamarone34@gmail.com>
2022-08-11 15:35:31 -0300

    refactor

--- script.sh
+++ script.sh
@@ -9,17 +9,17 @@ groupadd GPR_VEN
 groupadd GPR_SEC
 echo "criando usuários"
 #admins
-useradd carlos -c "Carlos" -m -p $(openssl passwd -crypt Senha123) -g GPR_ADM
-useradd maria -c "Maria" -m -p $(openssl passwd -crypt Senha123) -g GPR_ADM
-useradd joao_ -c "Joao" -m -p $(openssl passwd -crypt Senha123) -g GPR_ADM
+useradd carlos -c "Carlos" -m -p $(openssl passwd -crypt Senha123) -s /bin/bash -g GPR_ADM
+useradd maria -c "Maria" -m -p $(openssl passwd -crypt Senha123) -s /bin/bash -g GPR_ADM
+useradd joao_ -c "Joao" -m -p $(openssl passwd -crypt Senha123) -s /bin/bash -g GPR_ADM
 #ven
-useradd debora -c "Débora" -m -p $(openssl passwd -crypt Senha123) -g GPR_VEN
-useradd sebastiana -c "Sebastiana" -m -p $(openssl passwd -crypt Senha123) -g GPR_VEN
-useradd roberto -c "Roberto" -m -p $(openssl passwd -crypt Senha123) -g GPR_VEN
+useradd debora -c "Débora" -m -p $(openssl passwd -crypt Senha123) -s /bin/bash -G GPR_VEN
+useradd sebastiana -c "Sebastiana" -m -p $(openssl passwd -crypt Senha123) -s /bin/bash -g GPR_VEN
+useradd roberto -c "Roberto" -m -p $(openssl passwd -crypt Senha123) -s /bin/bash -g GPR_VEN
 #sec
-useradd josefina -c "Josefina" -m -p $(openssl passwd -crypt Senha123) -g GPR_SEC
-useradd amanda -c "Amanda" -m -p $(openssl passwd -crypt Senha123) -g GPR_SEC
-useradd rogerio -c "Rogerio" -m -p $(openssl passwd -crypt Senha123) -g GPR_SEC
+useradd josefina -c "Josefina" -m -p $(openssl passwd -crypt Senha123) -s /bin/bash -g GPR_SEC
+useradd amanda -c "Amanda" -m -p $(openssl passwd -crypt Senha123) -s /bin/bash -g GPR_SEC
+useradd rogerio -c "Rogerio" -m -p $(openssl passwd -crypt Senha123) -s /bin/bash -g GPR_SEC
 
 echo "mudando dono das pastas"
 chown root:GPR_ADM /adm 
@@ -27,8 +27,8 @@ chown root:GPR_VEN /ven
 chown root:GPR_SEC /sec 
 
 echo "mudando permissão das pastas"
-chmod 770 /adm
-chmod 770 /ven
-chmod 770 /sec
+chmod 770 /adm -R
+chmod 770 /ven -R
+chmod 770 /sec -R
 
 chmod 777 /publico
